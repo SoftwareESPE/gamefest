08b40090c6d7bb72dd0843f8d2a06c4f91877d9b
Josue Erazo <jderazo4@espe.edu.ec>
2017-07-31 20:35:51 -0500

    Listo para laborar

--- src/baseDatos/Tareas.java
+++ src/baseDatos/Tareas.java
@@ -1,6 +1,17 @@
 
 package baseDatos;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.Blob;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
 
 
 
